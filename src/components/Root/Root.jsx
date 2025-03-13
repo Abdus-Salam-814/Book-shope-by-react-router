530d738e86a83cf15c7879aecd1f7fbd7c888481
slam814 <abdussalam6111997@gmail.com>
2025-03-13 16:57:52 +0600

    i setup layout reactRouter

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import NavBar from "../NavBar/NavBar";
 
 const Root = () => {
     return (
-        <div>
-            <h1>Hello Root</h1>
+        <div className="container mx-auto">
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
